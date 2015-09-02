2a9d18acd02f8b60ee95476b3db5c214b0f64a26
Ian Knox <knoxid@gmail.com>
2015-09-01 22:49:38 -0600

    added default loc

--- src/main/webapp/assets/javascripts/application.js
+++ src/main/webapp/assets/javascripts/application.js
@@ -1,26 +1,34 @@
 $(document).ready(function () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getOrigin);
-    } else {
-        localStorage.setItem('userOrigin', '')
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getOrigin);
+  } else {
+    localStorage.setItem('userOrigin', '')
+  }
 
-    function getOrigin(origin) {
-        var originLat = origin.coords.latitude;
-        var originLong = origin.coords.longitude;
+  function getOrigin(origin) {
+    var originLat = origin.coords.latitude;
+    var originLong = origin.coords.longitude;
 
-        localStorage['userLat'] = originLat;
-        localStorage['userLng'] = originLong;
-    }
+    localStorage['userLat'] = originLat;
+    localStorage['userLng'] = originLong;
+  }
 
-    var lat = (typeof localStorage['userLat'] == 'string') ? localStorage['userLat'] : 40;
-    var lng = (typeof localStorage['userLng'] == 'string') ? localStorage['userLng'] : -70;
+  var lat, lng, zoom;
 
-    var token = $('#mainMap').data('maptoken');
-    L.mapbox.accessToken = token;
+  if (typeof localStorage['userLat'] == 'string') {
+    lat = localStorage['userLat'];
+    lng = localStorage['userLng'];
+    zoom = 15
+  } else {
+    lat = 39.83;
+    lng = -98.58;
+    zoom = 5
+  }
+  var token = $('#mainMap').data('maptoken');
+  L.mapbox.accessToken = token;
 
-    var map = L.mapbox.map('mainMap', 'mapbox.streets')
-        .setView([lat, lng], 15);
+  var map = L.mapbox.map('mainMap', 'mapbox.streets')
+    .setView([lat, lng], zoom);
 
-    L.marker([lat, lng]).addTo(map)
+  L.marker([lat, lng]).addTo(map)
 });
